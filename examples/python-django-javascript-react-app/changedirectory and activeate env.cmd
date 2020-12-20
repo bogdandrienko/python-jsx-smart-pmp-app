c1cc854848377c09c243d6188419ca128a53294b
Bogdan <67187037+bogdandrienko@users.noreply.github.com>
2020-12-20 15:45:21 +0600

    update

--- examples/python-django-javascript-react-app/changedirectory and activeate env.cmd
+++ examples/python-django-javascript-react-app/changedirectory and activeate env.cmd
@@ -1,2 +1,2 @@
-cd C:\Projects\python-jsx-smart-pmp-app\examples\python-django-javascript-react-app
-C:\Projects\python-jsx-smart-pmp-app\examples\python-django-javascript-react-app\env\Scripts\activate.bat
+cd C:\Project\Github Projects\python-jsx-smart-pmp-app\examples\python-django-javascript-react-app
+C:\Project\Github Projects\python-jsx-smart-pmp-app\examples\python-django-javascript-react-app\env\Scripts\activate.bat
