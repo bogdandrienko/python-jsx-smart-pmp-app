aa13ec741208ee74b310962c21434c55436b9cf1
Bogdan <67187037+bogdandrienko@users.noreply.github.com>
2021-02-01 16:58:42 +0600

    update

--- EXAMPLES/old/ecommerce-web-app/shop/views.py
+++ EXAMPLES/old/ecommerce-web-app/shop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Category, Product, Cart, CartItem
 
 
@@ -37,6 +37,20 @@ def _cart_id(request):
 
 def add_cart(request, product_id):
 	product = Product.objects.get(id=product_id)
+	try:
+		cart = Cart.objects.get(cart_id=_cart_id(request))
+	except Cart.DoesNotExist:
+		cart = Cart.objects.create(cart_id=_cart_id(request))
+		cart.save()
+	try:
+		cart_item = CartItem.objects.get(product=product, cart=cart)
+		cart_item.quantity +=1
+		cart_item.save()
+	except CartItem.DoesNotExist:
+		cart_item = CartItem.objects.create(product=product, quantity=1, cart=cart)
+		cart_item.save()
+	
+	return redirect('cart_detail')
 
 
 
