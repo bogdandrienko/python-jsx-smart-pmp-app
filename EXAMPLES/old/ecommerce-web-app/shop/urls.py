c0594af23ba5d14422892a895c415470c9c09a2e
Bogdan <67187037+bogdandrienko@users.noreply.github.com>
2021-02-01 15:01:02 +0600

    update

--- EXAMPLES/old/ecommerce-web-app/shop/urls.py
+++ EXAMPLES/old/ecommerce-web-app/shop/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('category/', views.category, name='category'),
     path('category/<slug:category_slug>', views.category, name='products_by_category'),
     path('category/<slug:category_slug>/<slug:product_slug>', views.product, name='products_detail'),
+    path('cart/', views.cart, name='cart'),
 ]
--- EXAMPLES/old/ecommerce-web-app/shop/views.py
+++ EXAMPLES/old/ecommerce-web-app/shop/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import get_object_or_404, render
-from .models import Category, Product
+from .models import Category, Product, Cart, CartItem
 
 
 # Create your views here.
+
 
 def category(request, category_slug=None):
 	category_page = None
@@ -14,9 +15,29 @@ def category(request, category_slug=None):
 		products = Product.objects.all().filter(available=True)
 	return render(request, 'category.html', {'category':category_page, 'products': products})
 
+
 def product(request, category_slug, product_slug):
 	try:
 		products = Product.objects.get(category__slug=category_slug, slug=product_slug)
 	except Exception as e:
 		raise e
 	return render(request, 'product.html', {'products': products})
+
+
+def cart(request):
+	return render(request, 'cart.html')
+
+
+def _cart_id(request):
+	cart = request.session.session_key
+	if not cart:
+		cart = request.session.create
+	return cart
+
+
+def add_cart(request, product_id):
+	product = Product.objects.get(id=product_id)
+
+
+
+
