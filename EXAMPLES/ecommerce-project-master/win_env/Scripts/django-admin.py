6b5c5fd19b3eeb13b7b3476b88da15766f3eb937
Bogdan <67187037+bogdandrienko@users.noreply.github.com>
2021-02-07 15:50:16 +0600

    update

---
+++ EXAMPLES/ecommerce-project-master/win_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\project\github_projects\python-jsx-smart-pmp-app\examples\ecommerce-project-master\win_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
