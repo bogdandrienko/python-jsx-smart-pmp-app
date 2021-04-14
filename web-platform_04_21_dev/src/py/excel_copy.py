ca29c8777ceb164e5bfac5a0ccf79cc42f6b8cb3
Bogdan <67187037+bogdandrienko@users.noreply.github.com>
2021-04-14 19:34:12 +0600

    Update excel_copy.py

--- web-platform_04_21_dev/src/py/excel_copy.py
+++ web-platform_04_21_dev/src/py/excel_copy.py
@@ -1,41 +1,46 @@
+
 import openpyxl
-from openpyxl.descriptors.base import String
 
 old_path = 'C:\\Users\\bogdan\\Desktop\\new\\'
 old_file = 'Export.xlsx'
 new_path = 'C:\\Users\\bogdan\\Desktop\\new\\'
 new_file = 'Import.xlsx'
+A_1 = []
+C_1 = []
+ID_1 = []
+ID_2 = []
 
 def get_sheet_value(column = str, row = int):
     return str(sheet[str(str(column)+str(row))].value)
 
 workbook = openpyxl.load_workbook(old_path+old_file)
 sheet = workbook.active
-A_1 = ['']
-B_1 = ['']
-G_1 = ['']
-for row in range(1, 2155):
-    A_1.append(get_sheet_value('A', row))
-    B_1.append(get_sheet_value('B', row))
-    G_1.append(get_sheet_value('G', row))
+for num in range(1, 2155):
+    A_1.append(get_sheet_value('A', num))
+    C_1.append(get_sheet_value('C', num))
+    ID_1.append(get_sheet_value('G', num))
+
 workbook = openpyxl.load_workbook(new_path+new_file)
 sheet = workbook.active
-C_2 = ['']
-for row in range(14, 1388):
-    C_2.append(get_sheet_value('C', row))
+for num in range(1, 1387):
+    ID_2.append(get_sheet_value('C', num))
 
-
-for id_1 in G_1:
-    for id_2 in C_2:
+for id_1 in ID_1:
+    value_1 = ID_1.index(id_1)
+    for id_2 in ID_2:
+        value_2 = ID_2.index(id_2)+1
         if id_1 == id_2:
-            sheet['R'+str(id_2)] = B_1.index(id_1)
-
-
-            # if B_1[G_1.index(id_1)] == '' or B_1[G_1.index(id_1)] == 'None':
-            #     sheet[str(str('R')+str(id_2))] = ''
-            # else:
-            #     sheet[str(str('R')+str(id_2))] = B_1[id_1]
-
-
+            try:
+                if C_1[value_1] == 'None' or C_1[value_1] == '':
+                    sheet['R'+str(value_2)] = ''
+                else:
+                    sheet['R'+str(value_2)] = C_1[value_1]
+                if A_1[value_1] == 'None' or A_1[value_1] == '':
+                    sheet['S'+str(value_2)] = ''
+                else:
+                    sheet['S'+str(value_2)] = A_1[value_1]
+                print('.')
+            except:
+                print('error')
 
 workbook.save(new_path+new_file)
