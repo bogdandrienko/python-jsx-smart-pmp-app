d5c7f8ba1dfc639913e8ba32867ff3cb2e906a6d
Bogdan <67187037+bogdandrienko@users.noreply.github.com>
2021-04-15 14:12:17 +0600

    Update excel_copy.py

--- web-platform_04_21_dev/src/py/excel_copy.py
+++ web-platform_04_21_dev/src/py/excel_copy.py
@@ -1,9 +1,8 @@
-
 import openpyxl
 
-old_path = 'C:\\Users\\bogdan\\Desktop\\new\\'
+old_path = 'C:\\Users\\bogdan\\Desktop\\СКУД\\Добавление и удаление персонала\\'
 old_file = 'Export.xlsx'
-new_path = 'C:\\Users\\bogdan\\Desktop\\new\\'
+new_path = 'C:\\Users\\bogdan\\Desktop\\СКУД\\Добавление и удаление персонала\\'
 new_file = 'Import.xlsx'
 A_1 = []
 C_1 = []
@@ -15,32 +14,32 @@ def get_sheet_value(column = str, row = int):
 
 workbook = openpyxl.load_workbook(old_path+old_file)
 sheet = workbook.active
-for num in range(1, 2155):
+for num in range(1, 2156):
     A_1.append(get_sheet_value('A', num))
     C_1.append(get_sheet_value('C', num))
     ID_1.append(get_sheet_value('G', num))
 
 workbook = openpyxl.load_workbook(new_path+new_file)
 sheet = workbook.active
-for num in range(1, 1387):
+for num in range(1, 1388):
     ID_2.append(get_sheet_value('C', num))
 
+i = 1
 for id_1 in ID_1:
-    value_1 = ID_1.index(id_1)
     for id_2 in ID_2:
-        value_2 = ID_2.index(id_2)+1
         if id_1 == id_2:
             try:
-                if C_1[value_1] == 'None' or C_1[value_1] == '':
-                    sheet['R'+str(value_2)] = ''
+                if C_1[ID_1.index(id_1)] == 'None' or C_1[ID_1.index(id_1)] == '':
+                    sheet['R'+str(ID_2.index(id_2)+1)] = ''
                 else:
-                    sheet['R'+str(value_2)] = C_1[value_1]
-                if A_1[value_1] == 'None' or A_1[value_1] == '':
-                    sheet['S'+str(value_2)] = ''
+                    sheet['R'+str(ID_2.index(id_2)+1)] = C_1[ID_1.index(id_1)]
+                if A_1[ID_1.index(id_1)] == 'None' or A_1[ID_1.index(id_1)] == '':
+                    sheet['S'+str(ID_2.index(id_2)+1)] = ''
                 else:
-                    sheet['S'+str(value_2)] = A_1[value_1]
-                print('.')
+                    sheet['S'+str(ID_2.index(id_2)+1)] = A_1[ID_1.index(id_1)]
+                print(i)
+                i = i+1
             except:
                 print('error')
-
+sheet['S'+str(1)] = ''
 workbook.save(new_path+new_file)
