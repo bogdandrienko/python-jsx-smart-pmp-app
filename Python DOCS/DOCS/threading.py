b66e753d0b369188d269ea71fd2a44c53dedb377
Bogdan <67187037+bogdandrienko@users.noreply.github.com>
2021-04-30 14:27:47 +0600

    update

---
+++ Python DOCS/DOCS/threading.py
@@ -0,0 +1,15 @@
+import time
+import threading
+import multiprocessing
+
+a = threading.Barrier()
+b = multiprocessing.Barrier()
+
+class HorseRace:
+    def __init__(self):
+        self.barrier = threading.lock()
+        self.horses = ['Artax', 'Frankel', 'Bucephalus', 'Barton']
+
+    def lead(self):
+        horse = self.horses.pop()
+        time.sleep(1.5)
