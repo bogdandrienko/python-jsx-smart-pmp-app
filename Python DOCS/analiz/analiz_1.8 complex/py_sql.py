199388c3a6d8fbfe52201b7e527b4df7439f04dc
Bogdan <67187037+bogdandrienko@users.noreply.github.com>
2021-05-23 11:30:22 +0600

    update

---
+++ Python DOCS/analiz/analiz_1.8 complex/py_sql.py
@@ -0,0 +1,63 @@
+import pyodbc
+import pandas
+
+
+class SQLClass:
+    @staticmethod
+    def sql_post_data(ip: str, server: str, port: str, database: str, username: str, password: str, table: str,
+                      rows: list, values: list):
+        try:
+            sql = SQLClass.pyodbc_connect(ip=ip, server=server, port=port, database=database, username=username,
+                                          password=password)
+            SQLClass.execute_data_query(connection=sql, table=table, rows=rows, values=values)
+        except Exception as ex:
+            print(ex)
+            with open('log.txt', 'a') as log:
+                log.write(f'\n{ex}\n')
+
+    @staticmethod
+    def sql_post_now(ip: str, server: str, port: str, database: str, username: str, password: str, table: str,
+                     rows: list, values: list):
+        try:
+            sql = SQLClass.pyodbc_connect(ip=ip, server=server, port=port, database=database, username=username,
+                                          password=password)
+            SQLClass.execute_now_query(connection=sql, table=table, rows=rows, values=values)
+        except Exception as ex:
+            print(ex)
+            with open('log.txt', 'a') as log:
+                log.write(f'\n{ex}\n')
+
+    @staticmethod
+    def pyodbc_connect(ip: str, server: str, port: str, database: str, username: str, password: str):
+        conn_str = (
+                r'DRIVER={ODBC Driver 17 for SQL Server};SERVER=tcp:' + ip + '\\' + server + ',' + port +
+                ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password + ';'
+        )
+        return pyodbc.connect(conn_str)
+
+    @staticmethod
+    def pd_read_sql_query(connection, query: str, database: str, table: str):
+        return pandas.read_sql_query(f'{query} {database}.dbo.{table} ORDER BY id', connection)
+
+    @staticmethod
+    def execute_data_query(connection, table: str, rows: list, values: list):
+        cursor = connection.cursor()
+        cursor.fast_executemany = True
+        _rows = ''
+        for x in rows:
+            _rows = f"{_rows}{str(x)}, "
+        value = f"INSERT INTO {table} (" + _rows[:-2:] + f") VALUES {tuple(values)}"
+        cursor.execute(value)
+        connection.commit()
+
+    @staticmethod
+    def execute_now_query(connection, table, rows: list, values: list):
+        cursor = connection.cursor()
+        cursor.fast_executemany = True
+        __rows = ''
+        for x in rows:
+            __rows = f"{__rows}{str(x)}, "
+        value = f"UPDATE {table} SET {rows[1]} = '{values[1]}',{rows[2]} = '" \
+                f"{values[2]}' WHERE {rows[0]} = '{values[0]}'"
+        cursor.execute(value)
+        connection.commit()
