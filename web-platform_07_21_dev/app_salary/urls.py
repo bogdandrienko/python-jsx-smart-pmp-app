2d536106e34ab145ac287952cfa71e54fdf3a165
Bogdan <67187037+bogdandrienko@users.noreply.github.com>
2021-07-22 13:25:51 +0600

    update

--- web-platform_07_21_dev/app_salary/urls.py
+++ web-platform_07_21_dev/app_salary/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'app_salary'
 urlpatterns = [
     path('', views.salary, name='salary'),
+    path('view_pdf/', views.view_pdf, name='view_pdf'),
+    path('create_pdf/', views.render_pdf_view, name='create_pdf'),
     path('check/<int:request_id>', views.salary, name='salary/check'),
 ]
--- web-platform_07_21_dev/app_salary/views.py
+++ web-platform_07_21_dev/app_salary/views.py
@@ -4,6 +4,81 @@ from django.http.response import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls.base import reverse
 from django.contrib.auth.models import User
+import os
+import io
+from django.conf import settings
+from django.http import HttpResponse
+from django.template.loader import get_template, render_to_string
+from xhtml2pdf import pisa
+from django.contrib.staticfiles import finders
+
+
+def link_callback(uri, rel):
+    """
+    Convert HTML URIs to absolute system paths so xhtml2pdf can access those
+    resources
+    """
+    result = finders.find(uri)
+    if result:
+        if not isinstance(result, (list, tuple)):
+            result = [result]
+        result = list(os.path.realpath(path) for path in result)
+        path = result[0]
+    else:
+        sUrl = settings.STATIC_URL  # Typically /static/
+        sRoot = settings.STATIC_ROOT  # Typically /home/userX/project_static/
+        mUrl = settings.MEDIA_URL  # Typically /media/
+        mRoot = settings.MEDIA_ROOT  # Typically /home/userX/project_static/media/
+
+        if uri.startswith(mUrl):
+            path = os.path.join(mRoot, uri.replace(mUrl, ""))
+        elif uri.startswith(sUrl):
+            path = os.path.join(sRoot, uri.replace(sUrl, ""))
+        else:
+            return uri
+
+    # make sure that file exists
+    if not os.path.isfile(path):
+        raise Exception(
+            'media URI must start with %s or %s' % (sUrl, mUrl)
+        )
+    return path
+
+
+def view_pdf(request):
+    data = data_s()
+    context = {
+        'data': data,
+    }
+    return render(request, 'app_salary/pdf.html', context)
+
+
+def render_pdf_view(request):
+    template_path = 'app_salary/pdf.html'
+    data = data_s()
+    context = {
+        'data': data,
+        'STATIC_ROOT': settings.STATIC_ROOT,
+    }
+    # Create a Django response object, and specify content_type as pdf
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="report.pdf"'
+    # find the template and render it.
+    template = get_template(template_path)
+    html = template.render(context)
+
+    # create a pdf
+    pisa_status = pisa.CreatePDF(
+        html, dest=response, encoding='utf-8', link_callback=link_callback)
+    # template = render_to_string(template_path, context)
+    # pdf = pisa.pisaDocument(io.BytesIO(template.encode('UTF-8')), response,
+    #                         encoding='utf-8',
+    #                         link_callback=link_callback)
+    # pdf = pisa.pisaDocument(io.StringIO(html), response, encoding='UTF-8')
+    # if error then show some funy view
+    if pisa_status.err:
+        return HttpResponse('We had some errors <pre>' + html + '</pre>')
+    return response
 
 
 def salary(request, request_id=0):
