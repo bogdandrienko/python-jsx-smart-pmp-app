9bffc480624278c389af1e7fb62175130d3aa4aa
Bogdan <67187037+bogdandrienko@users.noreply.github.com>
2022-02-24 22:03:38 +0600

    update

--- web-platform_22_03_dev/scripts/lin_2_create.sh
+++ web-platform_22_03_dev/scripts/lin_2_create.sh
@@ -11,13 +11,17 @@ pip install env
 source env/bin/activate
 
 # pip install --upgrade pip
-# python3 -m venv djangoenv
-# source djangoenv/bin/activate
+# python3 -m venv env
+# source env/bin/activate
+# sudo apt-get install unixodbc-dev
+# pip install pyodbc
+# pip install gunicorn
+# pip install -r requirements.txt
 # python manage.py makemigrations
 # python manage.py migrate
 
 # python manage.py runserver 0.0.0.0:8000
-# gunicorn --bind 0.0.0.0:8000 djangoproject.wsgi
+# gunicorn --bind 0.0.0.0:8000 backend.wsgi
 
 # sudo nano /etc/systemd/system/gunicorn.socket
 # sudo nano /etc/systemd/system/gunicorn.service
