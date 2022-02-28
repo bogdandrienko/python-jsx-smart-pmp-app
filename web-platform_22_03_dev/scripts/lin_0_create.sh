b70e213d4dca2faa5ac472ef561c2b150e05912e
Bogdan <67187037+bogdandrienko@users.noreply.github.com>
2022-02-28 16:53:26 +0600

    update

--- web-platform_22_03_dev/scripts/lin_0_create.sh
+++ web-platform_22_03_dev/scripts/lin_0_create.sh
@@ -162,7 +162,7 @@ server {
 listen 80;
 listen [::]:80;
 
-server_name 192.168.1.255;
+server_name web.km.kz;
 
 location /static/ {
     alias /home/bogdan/web/static/;
@@ -205,3 +205,64 @@ sudo systemctl reload nginx.service
 sudo reboot
 
 ########################################################################################################################
+
+sudo mv /etc/nginx/sites-available/example-http.conf /etc/nginx/sites-available/example-https.conf
+
+sudo nano /etc/nginx/sites-available/example-http.conf
+
+<file>
+server {
+listen 80;
+listen [::]:80;
+
+server_name web.km.kz;
+
+root /home/bogdan/web;
+
+location / {
+    return 301 https://$server_name$request_uri;
+}
+
+location /.well-known/acme-challenge/ {}
+}
+</file>
+
+########################################################################################################################
+
+sudo certbot certonly --webroot -w /home/bogdan/web -d web.km.kz -m bogdandrienko@gmail.com --agree-tos
+
+
+sudo nano /etc/nginx/sites-available/example-https.conf
+
+# listen 80; =>
+# listen [::]:80; =>
+
+<file>
+listen 443 ssl http2;
+listen [::]:443 ssl http2;
+
+ssl_certificate /etc/letsencrypt/live/web.km.kz/fullchain.pem;
+ssl_certificate_key /etc/letsencrypt/live/web.km.kz/privkey.pem;
+
+ssl_session_timeout 1d;
+ssl_session_cache shared:MozSSL:10m;
+
+ssl_dhparam /etc/nginx/dhparam.pem;
+
+ssl_protocols TLSv1.2;
+ssl_ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384;
+ssl_prefer_server_ciphers off;
+
+ssl_stapling on;
+ssl_stapling_verify on;
+
+ssl_trusted_certificate /etc/letsencrypt/live/web.km.kz/chain.pem;
+
+resolver 1.1.1.1;
+</file>
+
+sudo ln -s /etc/nginx/sites-available/example-https.conf /etc/nginx/sites-enabled/example-https.conf
+
+# sudo nginx -t
+
+sudo systemctl reload nginx.service
