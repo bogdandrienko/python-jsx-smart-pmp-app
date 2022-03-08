04ae78321922863beac9cd9cb1d560986d12ce99
Bogdan <67187037+bogdandrienko@users.noreply.github.com>
2022-03-08 23:47:57 +0600

    update

--- web-platform_22_03_dev/frontend/src/js/App.js
+++ web-platform_22_03_dev/frontend/src/js/App.js
@@ -28,9 +28,12 @@ import VacancyListPage from "../pages/vacancy/VacancyListPage";
 import VacancyCreatePage from "../pages/vacancy/VacancyCreatePage";
 import VacancyChangePage from "../pages/vacancy/VacancyChangePage";
 import VacancyDetailPage from "../pages/vacancy/VacancyDetailPage";
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 import ResumeListPage from "../pages/resume/ResumeListPage";
 import ResumeCreatePage from "../pages/resume/ResumeCreatePage";
 import ResumeDetailPage from "../pages/resume/ResumeDetailPage";
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+import IdeaCreatePage from "../pages/idea/IdeaCreatePage";
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 function App() {
@@ -77,6 +80,8 @@ function App() {
           <Route path="/resume_create/:id" element={<ResumeCreatePage />} />
           <Route path="/resume_list" element={<ResumeListPage />} />
           <Route path="/resume_detail/:id" element={<ResumeDetailPage />} />
+          //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+          <Route path="/idea_create" element={<IdeaCreatePage />} />
           //////////////////////////////////////////////////////////////////////////////////////////////////////////////
         </Routes>
       </div>
---
+++ web-platform_22_03_dev/frontend/src/pages/idea/IdeaCreatePage.js
@@ -0,0 +1,271 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+import * as constants from "../../js/constants";
+import * as actions from "../../js/actions";
+import * as utils from "../../js/utils";
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+import HeaderComponent from "../../components/HeaderComponent";
+import TitleComponent from "../../components/TitleComponent";
+import FooterComponent from "../../components/FooterComponent";
+import StoreStatusComponent from "../../components/StoreStatusComponent";
+import MessageComponent from "../../components/MessageComponent";
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const IdeaCreatePage = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const id = useParams().id;
+
+  const [qualification, qualificationSet] = useState("");
+  const [rank, rankSet] = useState("");
+  const [image, imageSet] = useState(null);
+  const [sphere, sphereSet] = useState("");
+  const [education, educationSet] = useState("");
+  const [experience, experienceSet] = useState("");
+  const [schedule, scheduleSet] = useState("");
+  const [description, descriptionSet] = useState("");
+
+  const vacancyCreateStore = useSelector((state) => state.vacancyCreateStore);
+  const {
+    // load: loadVacancyCreate,
+    data: dataVacancyCreate,
+    // error: errorVacancyCreate,
+    // fail: failVacancyCreate,
+  } = vacancyCreateStore;
+
+  useEffect(() => {
+    if (dataVacancyCreate) {
+      utils.Sleep(2000).then(() => {
+        dispatch({ type: constants.VACANCY_CREATE_RESET_CONSTANT });
+        dispatch({ type: constants.VACANCY_LIST_RESET_CONSTANT });
+        navigate("/vacancy_list");
+      });
+    }
+  }, [dataVacancyCreate]);
+
+  const formHandlerSubmit = (e) => {
+    e.preventDefault();
+    const form = {
+      "Action-type": "VACANCY_CREATE",
+      qualification: qualification,
+      rank: rank,
+      sphere: sphere,
+      education: education,
+      experience: experience,
+      schedule: schedule,
+      image: image,
+      description: description,
+    };
+    dispatch(actions.vacancyCreateAction(form));
+  };
+
+  return (
+    <div>
+      <HeaderComponent logic={true} redirect={true} />
+      <TitleComponent
+        first={"Создать вакансию"}
+        second={"страница с формой для создания новой свободной вакансии."}
+      />
+      <main className="container p-0">
+        <div className="m-0 p-0">
+          {StoreStatusComponent(
+            vacancyCreateStore,
+            "vacancyCreateStore",
+            true,
+            "",
+            constants.DEBUG_CONSTANT
+          )}
+        </div>
+        <div className="btn-group p-1 m-0 text-start w-100">
+          <Link to={"/vacancy_list"} className="btn btn-sm btn-primary">
+            {"<="} назад к списку
+          </Link>
+        </div>
+        <div className="container-fluid">
+          <ul className="row-cols-auto row-cols-md-auto row-cols-lg-auto justify-content-center p-0 m-0">
+            <form autoComplete="on" className="" onSubmit={formHandlerSubmit}>
+              <div className="p-0 m-0">
+                <h6 className="lead">Свободная вакансия</h6>
+              </div>
+              <div className="p-0 m-0">
+                <label className="w-50 form-control-md m-1">
+                  Квалификация:
+                  <input
+                    type="text"
+                    value={qualification}
+                    required
+                    minLength="1"
+                    maxLength="256"
+                    placeholder="вводите квалификацию тут..."
+                    className="form-control form-control-sm"
+                    onChange={(e) => qualificationSet(e.target.value)}
+                  />
+                  <small className="text-danger">* обязательно</small>
+                  <p className="p-0 m-0">
+                    <small className="text-muted">
+                      длина: не более 256 символов
+                    </small>
+                  </p>
+                </label>
+                <label className="form-control-md m-1">
+                  Разряд:
+                  <input
+                    type="text"
+                    placeholder="вводите разряд тут..."
+                    value={rank}
+                    minLength="1"
+                    maxLength="32"
+                    className="form-control form-control-sm"
+                    onChange={(e) => rankSet(e.target.value)}
+                  />
+                  <small className="text-secondary">* не обязательно</small>
+                  <p className="p-0 m-0">
+                    <small className="text-muted">
+                      длина: не более 32 символов
+                    </small>
+                  </p>
+                </label>
+              </div>
+              <br />
+              <div className="p-0 m-0">
+                <label className="form-control-md m-1">
+                  Изображение:
+                  <input
+                    type="file"
+                    accept=".jpg, .png"
+                    className="form-control form-control-sm"
+                    onChange={(e) => imageSet(e.target.files[0])}
+                  />
+                  <small className="text-muted">* не обязательно</small>
+                </label>
+                <label className="form-control-md m-1">
+                  Сфера:
+                  <select
+                    value={sphere}
+                    required
+                    className="form-control form-control-sm"
+                    onChange={(e) => sphereSet(e.target.value)}
+                  >
+                    <option value="">не выбрано</option>
+                    <option value="Технологическая">Технологическая</option>
+                    <option value="Не технологическая">
+                      Не технологическая
+                    </option>
+                  </select>
+                  <small className="text-danger">* обязательно</small>
+                </label>
+              </div>
+              <br />
+              <div className="p-0 m-0">
+                <label className="form-control-md m-1">
+                  Образование:
+                  <select
+                    value={education}
+                    required
+                    className="form-control form-control-sm"
+                    onChange={(e) => educationSet(e.target.value)}
+                  >
+                    <option value="">не выбрано</option>
+                    <option value="Высшее, Средне-специальное">
+                      Высшее / Средне-специальное
+                    </option>
+                    <option value="Высшее">Высшее</option>
+                    <option value="Средне-специальное">
+                      Средне-специальное
+                    </option>
+                    <option value="Среднее">Среднее</option>
+                  </select>
+                  <small className="text-danger">* обязательно</small>
+                </label>
+                <label className="form-control-md m-1">
+                  Опыт:
+                  <select
+                    value={experience}
+                    required
+                    className="form-control form-control-sm"
+                    onChange={(e) => experienceSet(e.target.value)}
+                  >
+                    <option value="">не выбрано</option>
+                    <option value="не имеет значения">не имеет значения</option>
+                    <option value="от 1 года до 3 лет">
+                      от 1 года до 3 лет
+                    </option>
+                    <option value="от 3 до 6 лет">от 3 до 6 лет</option>
+                    <option value="более 6 лет">более 6 лет</option>
+                  </select>
+                  <small className="text-danger">* обязательно</small>
+                </label>
+                <label className="form-control-md m-1">
+                  График:
+                  <select
+                    value={schedule}
+                    required
+                    className="form-control form-control-sm"
+                    onChange={(e) => scheduleSet(e.target.value)}
+                  >
+                    <option value="">не выбрано</option>
+                    <option value="Полный день(5/2)">Полный день</option>
+                    <option value="Сменный">Сменный</option>
+                    <option value="Удалённая работа">Удалённая работа</option>
+                    <option value="Особый">Особый</option>
+                  </select>
+                  <small className="text-danger">* обязательно</small>
+                </label>
+              </div>
+              <br />
+              <div className="p-0 m-0">
+                <label className="w-75 form-control-md m-1">
+                  Описание:
+                  <textarea
+                    id="short_description_char_field"
+                    name="short_description_char_field"
+                    placeholder="вводите описание тут..."
+                    value={description}
+                    minLength="1"
+                    maxLength="512"
+                    rows="2"
+                    className="form-control form-control-sm"
+                    onChange={(e) => descriptionSet(e.target.value)}
+                  />
+                  <small className="text-secondary">* не обязательно</small>
+                  <p className="p-0 m-0">
+                    <small className="text-muted">
+                      длина: не более 512 символов
+                    </small>
+                  </p>
+                </label>
+              </div>
+              <br />
+              <div className="container-fluid text-center">
+                <ul className="row row-cols-auto row-cols-md-auto row-cols-lg-auto nav justify-content-center">
+                  <li className="m-1">
+                    <button
+                      className="btn btn-sm btn-outline-primary"
+                      type="submit"
+                    >
+                      Отправить
+                    </button>
+                  </li>
+                  <li className="m-1">
+                    <button
+                      className="btn btn-sm btn-outline-warning"
+                      type="reset"
+                    >
+                      Сбросить все данные
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            </form>
+          </ul>
+        </div>
+      </main>
+      <FooterComponent />
+    </div>
+  );
+};
+
+export default IdeaCreatePage;
